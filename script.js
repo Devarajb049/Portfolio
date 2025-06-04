15005ae120310ed1f7212699126731b3fd6d0496
Devarajb049 <bhojanapudevaraj@gmail.com>
2025-06-04 10:35:16 +0530

    formatted

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-  AOS.init({
-    duration: 1200,  // Animation duration in ms
-    once: false,      // Animation only occurs once
-  });
+AOS.init({
+  duration: 1200, // Animation duration in ms
+  once: false, // Animation only occurs once
+});
