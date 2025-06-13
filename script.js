3aa74d25786a90f8b46f138de84ac8898fe60fd0
Devarajb049 <bhojanapudevaraj@gmail.com>
2025-06-13 16:24:54 +0530

    added Certificates and Certificates page

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ AOS.init({
   duration: 1200, // Animation duration in ms
   once: false, // Animation only occurs once
 });
+
+function openFullscreen(id) {
+  const img = document.getElementById(id);
+  if (img.requestFullscreen) {
+    img.requestFullscreen();
+  } else if (img.webkitRequestFullscreen) {
+    img.webkitRequestFullscreen();
+  } else if (img.msRequestFullscreen) {
+    img.msRequestFullscreen();
+  }
+}
